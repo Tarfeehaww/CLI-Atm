d2224fd262331221b6b425a2d9d3c2dff0f4e515
TarfeehaHussain <164815147+Tarfeehaww@users.noreply.github.com>
2024-05-04 14:47:33 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+
+let myBalance = 10000; //dollars
+let myPin = 1234;
+
+let pinAnswer:any = await inquirer.prompt( 
+   [
+    {
+        name : "pin",
+        message : "enter your pin",
+        type: "number"
+    }
+]
+)
+
+if ( pinAnswer.pin === myPin) {
+    console.log("Correct PINCODE:" ,myPin)
+}
+else{  
+    console.log("incorrect PINCODE");
+
+};
+
+let operationAns =await inquirer.prompt([
+ 
+    {
+        name:"operation",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw", "check balance"]
+
+
+    }
+]);
+
+if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt(
+        [
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+
+
+
+
+
+            }
+        ]
+        );
+       myBalance -= amountAns.amount
+       console.log("your remaining balance is" , myBalance)
+} else if(operationAns.operation === "check balance") {
+    console.log("your balance is:" + myBalance )
+}
+
